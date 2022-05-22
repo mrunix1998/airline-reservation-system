9e15f3696213f5cf068058fd158a07a820fd65bb
Fateme Rezaeian <fatemerezaeian2976@gmail.com>
2022-05-22 23:26:22 +0430

    + add stub

--- src/test/java/com/examples/flightbooking/rest/stubs/CustomerRepoStub.java
+++ src/test/java/com/examples/flightbooking/rest/stubs/CustomerRepoStub.java
@@ -7,10 +7,22 @@ import java.util.Optional;
 
 public class CustomerRepoStub implements CustomerRepository {
 
+
+    public String get_fateme() {
+        String fateme = "{\"email\": \"fateme@email.com\", \"password\": \"1111\"}";
+        return fateme;
+    }
+
+    public String get_all_customers() {
+        String customer_list = new String("[{\"customerId\":1,\"firstName\":\"Fateme\",\"lastName\":\"Rezaeian\",\"email\":\"fateme@email.com\",\"password\":\"1111\"},{\"customerId\":2,\"firstName\":\"Mohammad\",\"lastName\":\"Salehi\",\"email\":\"h.mammad@email.com\",\"password\":\"1111\"},{\"customerId\":3,\"firstName\":\"Mahdi\",\"lastName\":\"Kaveh\",\"email\":\"kaveh@email.com\",\"password\":\"1111\"},{\"customerId\":4,\"firstName\":\"John\",\"lastName\":\"Doe\",\"email\":\"john@email.com\",\"password\":\"1111\"},{\"customerId\":5,\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"email\":\"jane@email.com\",\"password\":\"1111\"}]");
+        return customer_list;
+    }
+
     @Override
     public Customer findByEmailIgnoreCase(String email) {
         return null;
     }
+    String expected = new String("[{\"customerId\":1,\"firstName\":\"Fateme\",\"lastName\":\"Rezaeian\",\"email\":\"fateme@email.com\",\"password\":\"1111\"},{\"customerId\":2,\"firstName\":\"Mohammad\",\"lastName\":\"Salehi\",\"email\":\"h.mammad@email.com\",\"password\":\"1111\"},{\"customerId\":3,\"firstName\":\"Mahdi\",\"lastName\":\"Kaveh\",\"email\":\"kaveh@email.com\",\"password\":\"1111\"},{\"customerId\":4,\"firstName\":\"John\",\"lastName\":\"Doe\",\"email\":\"john@email.com\",\"password\":\"1111\"},{\"customerId\":5,\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"email\":\"jane@email.com\",\"password\":\"1111\"}]");
 
     @Override
     public void deleteCustomerByCustomerId(Integer customerId) {
